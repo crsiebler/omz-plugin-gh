760880159a05dcce2147e65827407de20d5dc893
Cory Siebler <cory.siebler@protonmail.com>
2025-03-12 13:37:53 -0700

    feat(gh.plugin.zsh): adding more recommend alias (#2)

--- gh.plugin.zsh
+++ gh.plugin.zsh
@@ -6,6 +6,8 @@ alias ghal='gh auth login'
 # Copilot
 alias ghcpe='gh copilot explain'
 alias ghcps='gh copilot suggest'
+alias ghcpd='gh copilot debug'
+alias ghcpc='gh copilot configure'
 
 # PR
 alias ghpr='gh pr create'
@@ -14,4 +16,14 @@ alias ghprc='gh pr checkout'
 alias ghprm='gh pr merge'
 alias ghprcmt='gh pr comment'
 alias ghprsts='gh pr status'
+
+# Repo
+alias ghr='gh repo create'
+alias ghrl='gh repo list'
+alias ghrcl='gh repo clone'
+
+# Issue
+alias ghi='gh issue create'
+alias ghil='gh issue list'
+alias ghic='gh issue comment'
 
